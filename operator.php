38815997965609e28e5b219feb29c4e1ed79c5b9
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-09-19 08:59:50 +0700

    praktikum3-2_pertemuan4

--- operator.php
+++ operator.php
@@ -14,6 +14,20 @@ echo "Hasil Kurang : {$hasilKurang} <br>";
 echo "Hasil Kali : {$hasilKali} <br>";
 echo "Hasil Bagi : {$hasilBagi} <br>";
 echo "Sisa Bagi : {$sisaBagi} <br>";
-echo "Pangkat : {$pangkat} <br>";
+echo "Pangkat : {$pangkat} <br> <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil Sama : {$hasilSama} <br>";
+echo "Hasil Tidak Sama : {$hasilTidakSama} <br>";
+echo "Hasil Lebih Kecil : {$hasilLebihKecil} <br>";
+echo "Hasil Lebih Besar : {$hasilLebihBesar} <br>";
+echo "Hasil Lebih Kecil Sama : {$hasilLebihKecilSama} <br>";
+echo "Hasil Lebih Besar Sama : {$hasilLebihBesarSama} <br>";
 
 ?>
