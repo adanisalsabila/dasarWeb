35e66fc61c86cd9afb3a2a6ae8153dc395d171d9
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-09-26 08:15:47 +0700

    soal7-pertemuan5

--- rekursif.php
+++ rekursif.php
@@ -24,17 +24,12 @@ $menu = [
         "nama" => "Kontak"
     ]
 ];
-
-function tampilkanMenuBertingkat(array $menu) {
-    echo "<ul>";
-    foreach ($menu as $item) {
-        echo "<li>{$item['nama']}";
-        if (isset($item['subMenu'])) {
-            tampilkanMenuBertingkat($item['subMenu']);
-        }
-        echo "</li>";
-    }
-    echo "</ul>";
-}
-
-tampilkanMenuBertingkat($menu);
+            function tampilkanMenuBertingkat (array $menu){
+                echo "<ul>";
+                foreach ($menu as $key => $sistem) {
+                    echo "<li>{$item['nama']}</li>";
+                }
+                echo "</ul>";
+            }
+            tampilkanMenuBertingkat($menu);
+?>
