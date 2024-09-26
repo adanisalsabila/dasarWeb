0fbeb7cb263039b3e1fe16b5e93c6eb0e290caac
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-09-26 08:12:06 +0700

    soal7-pertemuan5

--- rekursif.php
+++ rekursif.php
@@ -1,11 +1,40 @@
 <?php
-for ($i=1; $i <= 25 ; $i++) { 
-    echo "Perulangan ke-{$i} <br>";
-}
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br>";
-    tampilkanHaloDunia();
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    ["nama" => "Pantai"],
+                    ["nama" => "Gunung"]
+                ]
+            ],
+            ["nama" => "Kuliner"],
+            ["nama" => "Hiburan"]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ]
+];
+
+function tampilkanMenuBertingkat(array $menu) {
+    echo "<ul>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}";
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+        echo "</li>";
+    }
+    echo "</ul>";
 }
 
-tampilkanHaloDunia();
-?>
+tampilkanMenuBertingkat($menu);
