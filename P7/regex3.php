79153c183a7dca8980dd527264efc34d32cce43f
adanisalsabilaaa@gmail.com <adanisalsabilaaa@gmail.com>
2024-10-17 07:53:31 +0700

    form lanjut

---
+++ P7/regex3.php
@@ -0,0 +1,25 @@
+<?php
+$pattern = '/[a-z]+/';
+$text = 'This is a sample text.';
+if(preg_match($pattern, $text)){
+    echo "Huruf kecil ditemukan!";
+}else{
+    echo "Tidak ada huruf kecil!";
+}
+echo "<br>";
+$pattern2 = '/[0-9]+/';
+$text2 = 'There are 123 apple.';
+if(preg_match($pattern2, $text2,$matches)){
+    echo "Cocokkan: " . $matches[0];
+}else{
+    echo "Tidak ada yang cocok!";
+}
+
+echo "<br>";
+
+$pattern3 = '/banana/';
+$replacement = 'banana';
+$text3 = 'I like apple pie';
+$new_text = preg_replace($pattern3,$replacement,$text3);
+echo $new_text;
+?>
